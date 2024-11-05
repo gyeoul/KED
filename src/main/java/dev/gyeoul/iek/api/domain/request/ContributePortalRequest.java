79a4d09feb4b48462ceb8559d0d920838c738354
Gyeoul <gyeoul@gyeoul.dev>
2024-11-05 21:39:08 +0900

    feat: Initial Commit
    - v0.0.4
    
    Signed-off-by: Gyeoul <gyeoul@gyeoul.dev>

--- src/main/java/dev/gyeoul/iek/api/domain/request/ContributePortalRequest.java
+++ src/main/java/dev/gyeoul/iek/api/domain/request/ContributePortalRequest.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 
 /*
  * guid  "5fd1eac6e681381e9dd9e050cb1bc175.16"
- * image "<a href="https://lh3.googleusercontent.com/5JwK5C_m8AXUovw1tuJGhWdjkgVlbuqv6yKeEYRZLZ_8SKWnPUsIqtmUqfVZ9KajP78xDV5ut2ohjbkU7rcXWapzmW9W">...</a>"
+ * image "https://lh3.googleusercontent.com/5JwK5C_m8AXUovw1tuJGhWdjkgVlbuqv6yKeEYRZLZ_8SKWnPUsIqtmUqfVZ9KajP78xDV5ut2ohjbkU7rcXWapzmW9W"
  * latE6 37519813
  * lngE6 126886469
  * title "한 마리 새처럼"
@@ -14,11 +14,11 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class ContributePortalRequest {
-    private String guid; // "12d4fbbd809d4cf484959dd35636ba91.16",
-    private long latE6; // 37516873,
-    private long lngE6; // 126926620,
-    private String title; // null,
-    private String image; // null
+    private String guid;
+    private long latE6;
+    private long lngE6;
+    private String title;
+    private String image;
 
     @Builder
     public ContributePortalRequest(String guid, long latE6, long lngE6, String title, String image) {
